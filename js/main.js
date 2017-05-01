1e1858a9a86a35c83a372b0e4cd369c9e035dfd1
李志鹏 <lzp0305@gmail.com>
2017-05-01 19:36:34 +0800

    2017-05-01

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+// 	var h = $('.ny-nr').innerHeight();
+// 	var hh = h+'px';
+// 	$('.zhezhao-all').css('height',hh);
+// })
+
+// $(document).ready(function(){
+// 	var h1 = $('.zhezhao-all').innerHeight();
+// 	var hh1 = h1+'px';
+// 	var h2 = $('.ny-nr').innerHeight();
+// 	var hh2 = h2+'px';
+// 	if (h1>h2) {
+// 		$('.ny-nr').css('height',hh1);
+// 	} else {
+// 		$('.zhezhao-all').css('height',hh2);
+// 	}
+// })
